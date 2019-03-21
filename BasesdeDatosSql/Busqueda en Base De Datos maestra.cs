5e3045906b58f0ee86a2427de142441f8b64b6e9
Jcarrazco <48524432+Jcarrazco@users.noreply.github.com>
2019-03-21 11:46:00 -0600

    Lista de tablas vistas y SP
    se termina el programa con las listas de los elementos de cada base de datos, se selecciona en la primer lista y muestra los datos de cada base de datos

---
+++ BasesdeDatosSql/Busqueda en Base De Datos maestra.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasesdeDatosSql
+{
+    public partial class BusquedaMaster : Form
+    {
+        public BusquedaMaster()
+        {
+            InitializeComponent();
+        }
+
+        List<string> listanew;
+        string conexion = "data source=HGDLAPCARRASCOJ\\SQLEXPRESS;" +
+            "initial catalog=master;integrated security = true;user id=sa";
+
+        private void Btn_return_Click(object sender, EventArgs e)
+        {
+            Vista_inicial ret = new Vista_inicial();
+            ret.Show();
+            this.Close();
+        }
+
+        private void BusquedaMaster_Load(object sender, EventArgs e)
+        {
+          
+            OperacionesDB Opdb = new OperacionesDB(conexion);
+
+            listanew = Opdb.BasesD();
+            foreach(string s in listanew)
+            {
+                Lstb_BD.Items.Add(s);
+            }
+            
+        }
+
+        private void Lstb_BD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string nombreDB = Lstb_BD.SelectedItem.ToString();
+            OperacionesDB Opdb = new OperacionesDB(conexion);//crea una conexion 
+            //---------------------carga info a Tablas-------------------------
+            listanew = Opdb.DatosBDTablas(nombreDB);//pide las tablas de la BD
+            Lstb_Tables.Items.Clear();//limpia
+            foreach (string s in listanew)//carga a la lista los datos TABLAS
+            {
+                Lstb_Tables.Items.Add(s);
+            }
+            //------------------Carga info a Views------------------------------------------
+            listanew = Opdb.DatosBDViews(nombreDB);//pide las tablas de la BD
+            Lstb_View.Items.Clear();//limpia
+            foreach (string s in listanew)//carga a la lista los datos TABLAS
+            {
+                Lstb_View.Items.Add(s);
+            }
+            //------------------Carga info a Stored procedures----------------------------
+            listanew = Opdb.DatosBDSP(nombreDB);//pide las tablas de la BD
+            Lstb_SP.Items.Clear();//limpia
+            foreach (string s in listanew)//carga a la lista los datos TABLAS
+            {
+                Lstb_SP.Items.Add(s);
+            }
+        }
+    }
+}
---
+++ BasesdeDatosSql/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BasesdeDatosSql
+{
+    partial class Vista_inicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV_Resultado = new System.Windows.Forms.DataGridView();
+            this.Btn_Load = new System.Windows.Forms.Button();
+            this.Btn_Buscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Resultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV_Resultado
+            // 
+            this.DGV_Resultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Resultado.Location = new System.Drawing.Point(12, 12);
+            this.DGV_Resultado.Name = "DGV_Resultado";
+            this.DGV_Resultado.Size = new System.Drawing.Size(410, 304);
+            this.DGV_Resultado.TabIndex = 0;
+            // 
+            // Btn_Load
+            // 
+            this.Btn_Load.Location = new System.Drawing.Point(466, 30);
+            this.Btn_Load.Name = "Btn_Load";
+            this.Btn_Load.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Load.TabIndex = 1;
+            this.Btn_Load.Text = "Load";
+            this.Btn_Load.UseVisualStyleBackColor = true;
+            this.Btn_Load.Click += new System.EventHandler(this.Btn_Load_Click);
+            // 
+            // Btn_Buscar
+            // 
+            this.Btn_Buscar.Location = new System.Drawing.Point(466, 267);
+            this.Btn_Buscar.Name = "Btn_Buscar";
+            this.Btn_Buscar.Size = new System.Drawing.Size(87, 49);
+            this.Btn_Buscar.TabIndex = 2;
+            this.Btn_Buscar.Text = "Buscar";
+            this.Btn_Buscar.UseVisualStyleBackColor = true;
+            this.Btn_Buscar.Click += new System.EventHandler(this.Btn_Buscar_Click);
+            // 
+            // Vista_inicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 328);
+            this.Controls.Add(this.Btn_Buscar);
+            this.Controls.Add(this.Btn_Load);
+            this.Controls.Add(this.DGV_Resultado);
+            this.Name = "Vista_inicial";
+            this.Text = "Vista de Datos";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Resultado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV_Resultado;
+        private System.Windows.Forms.Button Btn_Load;
+        private System.Windows.Forms.Button Btn_Buscar;
+    }
+}
+
